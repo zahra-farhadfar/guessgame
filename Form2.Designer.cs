5aaf06e83759de8d1eb941ebc5e72ddd2fa6b62c
zahra-farhadfar <89269362+zahra-farhadfar@users.noreply.github.com>
2021-08-20 21:18:40 +0430

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace $safeprojectname$
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.txtUserNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHighLow = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(927, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.Yellow;
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(364, 265);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(173, 98);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "START";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNewGame.Location = new System.Drawing.Point(305, 392);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(280, 73);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "NEW GAME";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // txtUserNumber
+            // 
+            this.txtUserNumber.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserNumber.Location = new System.Drawing.Point(494, 120);
+            this.txtUserNumber.Name = "txtUserNumber";
+            this.txtUserNumber.Size = new System.Drawing.Size(157, 89);
+            this.txtUserNumber.TabIndex = 3;
+            this.txtUserNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(150, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GUESS NUMBER:";
+            // 
+            // lblHighLow
+            // 
+            this.lblHighLow.AutoSize = true;
+            this.lblHighLow.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblHighLow.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighLow.ForeColor = System.Drawing.Color.Gray;
+            this.lblHighLow.Location = new System.Drawing.Point(571, 222);
+            this.lblHighLow.Name = "lblHighLow";
+            this.lblHighLow.Size = new System.Drawing.Size(0, 28);
+            this.lblHighLow.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(215, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "between 0 to 1000";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 503);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHighLow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserNumber);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.TextBox txtUserNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHighLow;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace $safeprojectname$
+{
+    public partial class Form2 : Form
+    {
+        int guessedNumber = 0;
+        static int computernumber = 0;
+        static string msg;
+        static int difference = 0;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            guessedNumber = Convert.ToInt32(txtUserNumber.Text);
+            if (Math.Abs(computernumber - guessedNumber) < difference)
+                txtUserNumber.BackColor = Color.Blue;
+            else
+                txtUserNumber.BackColor = Color.Red;
+            difference = computernumber - guessedNumber;
+
+            if (guessedNumber > computernumber)
+            {
+                msg = "too High";
+                txtUserNumber.Focus();
+            }
+
+            else if (guessedNumber < computernumber)
+            {
+                msg = "too Low";
+                txtUserNumber.Focus();
+            }
+            else
+            {
+                msg = "correct!";
+                txtUserNumber.BackColor = Color.Green;
+                btnGuess.Enabled = false;
+            }
+            lblHighLow.Text = msg;
+            difference = Math.Abs(computernumber - guessedNumber);
+
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+        private void init()
+        {
+            var rand = new Random();
+            computernumber = rand.Next(1, 1001);
+            btnGuess.Enabled = true;
+            txtUserNumber.Clear();
+            txtUserNumber.BackColor = Color.White;
+            lblHighLow.Text = "";
+            txtUserNumber.Focus();
+        }
+    }
+}
